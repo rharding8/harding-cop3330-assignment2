069a22a31c06e7979241b50d7a36412080922e7f
darkstar100x <darkstar100x@gmail.com>
2021-06-07 16:46:00 -0400

    Second Commit: Added ex24

--- src/main/java/ex24/base/AnagramChecker.java
+++ src/main/java/ex24/base/AnagramChecker.java
@@ -5,8 +5,44 @@
 
 package ex24.base;
 
+import java.util.Scanner;
+
 public class AnagramChecker {
+  Scanner input = new Scanner(System.in);
+
   public static void main(String[] args) {
-    System.out.println("Hello!");
+    AnagramChecker anagram = new AnagramChecker();
+    System.out.println("Enter two strings, and I shall tell you if they are anagrams.");
+    String wordA = anagram.getFirst();
+    String wordB = anagram.getSecond();
+    System.out.print("\"" + wordA + "\" and \"" + wordB + "\" are ");
+    if (!anagram.isAnagram(wordA, wordB)) {
+      System.out.print("not ");
+    }
+    System.out.println("anagrams.");
+  }
+
+  public String getFirst() {
+    System.out.print("Enter the first string: ");
+    return input.nextLine();
+  }
+
+  public String getSecond() {
+    System.out.print("Enter the second string: ");
+    return input.nextLine();
+  }
+
+  public boolean isAnagram(String wordA, String wordB) {
+    int i;
+    if (wordA.length() != wordB.length()) {
+      return false;
+    }
+
+    for (i = 0; i < wordA.length(); i++) {
+      if (!wordB.contains(""+wordA.charAt(i))) {
+        return false;
+      }
+    }
+    return true;
   }
 }
