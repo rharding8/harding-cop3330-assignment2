19fc82a0b47773751ac4772dee5493080787e37c
darkstar100x <darkstar100x@gmail.com>
2021-06-07 16:23:36 -0400

    Initial commit

---
+++ src/main/java/ex24/base/AnagramChecker.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Ryan Harding
+ */
+
+package ex24.base;
+
+public class AnagramChecker {
+  public static void main(String[] args) {
+    System.out.println("Hello!");
+  }
+}
