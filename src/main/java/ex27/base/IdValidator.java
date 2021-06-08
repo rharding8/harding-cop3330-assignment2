c699329a6f15781b5182090f6aa00b7fbb387352
darkstar100x <darkstar100x@gmail.com>
2021-06-08 18:35:16 -0400

    Fifth Commit: Added ex27

---
+++ src/main/java/ex27/base/IdValidator.java
@@ -0,0 +1,75 @@
+package ex27.base;
+
+public class IdValidator {
+  private String firstName;
+  private String lastName;
+  private String zipCode;
+  private String empID;
+
+  public IdValidator(String firstName, String lastName, String zipCode, String empID) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.zipCode = zipCode;
+    this.empID = empID;
+  }
+
+  public String validateInput() {
+    String msg = validateFirstName() + validateLastName() + validateZip() + validateEmp();
+    if (msg.equalsIgnoreCase("")) {
+      return "There were no errors found.";
+    }
+    return msg;
+  }
+
+  public String validateFirstName() {
+    if (firstName.equalsIgnoreCase("")) {
+      return String.format("The first name must be filled in.%n");
+    }
+    if (firstName.length() < 2) {
+      return String.format("\"%s\" is not a valid first name, it is too short.%n", firstName);
+    }
+    return "";
+  }
+
+  public String validateLastName() {
+    if (lastName.equalsIgnoreCase("")) {
+      return String.format("The last name must be filled in.%n");
+    }
+    if (lastName.length() < 2) {
+      return String.format("\"%s\" is not a valid last name, it is too short.%n", lastName);
+    }
+    return "";
+  }
+
+  public String validateZip() {
+    try {
+      Integer.parseInt(zipCode);
+    }
+    catch (NumberFormatException e) {
+      return String.format("The ZIP Code must be numeric.%n");
+    }
+    return "";
+  }
+
+  public String validateEmp() {
+    int i = 0;
+    while (i < 2) {
+      if (!Character.isLetter(empID.charAt(i))) {
+        return String.format("%s is not a valid ID.%n", empID);
+      }
+      i++;
+    }
+    if (empID.charAt(i) != '-') {
+      return String.format("%s is not a valid ID.%n", empID);
+    }
+    i++;
+    while (i < empID.length()) {
+      if (!Character.isDigit(empID.charAt(i))) {
+        return String.format("%s is not a valid ID.%n", empID);
+      }
+      i++;
+    }
+
+    return "";
+  }
+}
---
+++ src/main/java/ex27/base/Identification.java
@@ -0,0 +1,39 @@
+package ex27.base;
+
+import java.util.Scanner;
+
+public class Identification {
+  Scanner input = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    Identification id = new Identification();
+
+    String firstName = id.getFirstName();
+    String lastName = id.getLastName();
+    String zipCode = id.getZip();
+    String empID = id.getEmpID();
+
+    IdValidator valid = new IdValidator(firstName, lastName, zipCode, empID);
+    System.out.print(valid.validateInput());
+  }
+
+  private String getFirstName() {
+    System.out.print("Please enter a first name: ");
+    return input.nextLine();
+  }
+
+  private String getLastName() {
+    System.out.print("Please enter a last name: ");
+    return input.nextLine();
+  }
+
+  private String getZip() {
+    System.out.print("Please enter the ZIP code: ");
+    return input.nextLine();
+  }
+
+  private String getEmpID() {
+    System.out.print("Please enter the employee's ID: ");
+    return input.nextLine();
+  }
+}
