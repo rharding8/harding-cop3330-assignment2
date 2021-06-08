01a1d7f4aa5f8bdf2a411d031869baef27d58f01
darkstar100x <darkstar100x@gmail.com>
2021-06-07 22:09:34 -0400

    Fourth Commit: Added ex26

---
+++ src/main/java/ex26/base/CreditCard.java
@@ -0,0 +1,36 @@
+package ex26.base;
+
+import java.util.Scanner;
+
+public class CreditCard {
+  Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    CreditCard payment = new CreditCard();
+    double b = payment.setBal();
+    double apr = payment.setApr();
+    double p = payment.setMonthPay();
+    int n = payment.calculateMonthsUntilPaidOff(b, apr, p);
+    System.out.printf("It will take you %d months to pay off this credit card%n.", n);
+  }
+
+  public double setBal() {
+    System.out.print("What is your balance? ");
+    return input.nextDouble();
+  }
+
+  public double setApr() {
+    System.out.print("What is the APR on this card (%)? ");
+    return input.nextDouble();
+  }
+
+  public double setMonthPay() {
+    System.out.print("What is your monthly payment? ");
+    return input.nextDouble();
+  }
+
+  public int calculateMonthsUntilPaidOff(double bal, double apr, double monthPay) {
+    double i = (apr / 100) / 365;
+    double n = -(1.0/30.0) * Math.log10(1 + bal / monthPay * (1 - Math.pow(1 + i, 30))) / Math.log10(1 + i);
+    return (int) Math.ceil(n);
+  }
+}
